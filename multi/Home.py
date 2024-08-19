da4f2933640774f725ace6a9566a2e3fc40a6d89
Sagar Patil <patil_sag@xoriant.com>
2024-08-19 19:45:12 +0530

    testing delete button

--- multi/Home.py
+++ multi/Home.py
@@ -8,8 +8,7 @@ import time
 # load_dotenv()
 
 # Initialize OpenAI API
-api_key = your openai key
-openai.api_key = api_key
+api_key ='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'
 
 st.title(':red[Chatbot]')
 
